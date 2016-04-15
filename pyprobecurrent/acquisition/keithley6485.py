db4e85fac7a2f2973272857deec3ee174bd3806f
Hendrix Demers <12611589+drix00@users.noreply.github.com>
2016-04-15 10:45:01 -0400

    Add example of operation.

--- pyprobecurrent/acquisition/keithley6485.py
+++ pyprobecurrent/acquisition/keithley6485.py
@@ -35,7 +35,39 @@ class Keithley6485Picoammeter():
     def __init__(self, controller):
         self.controller = controller
         self.controller.gpib_usb.write("++addr 14")
-
+        self.controller.gpib_usb.write("*RST")
+    
+    def log_options(self):
+        data = self.controller.gpib_usb.query("SYST:LRF?")
+        logging.info("Line frequency: %s", data)
+        data = self.controller.gpib_usb.query("SYST:LRF:AUTO?")
+        logging.info("Line frequency auto: %s", data)
+        data = self.controller.gpib_usb.query("SYST:AZER?")
+        logging.info("Autozero: %s", data)
+        
+    def do_zero_correction(self):
+        self.controller.gpib_usb.write("*RST")
+        self.controller.gpib_usb.write("SYST:ZCH ON")
+        self.controller.gpib_usb.write("CURR:RANG 2E-4")
+        self.controller.gpib_usb.write("INIT")
+        self.controller.gpib_usb.write("SYST:ZCOR:ACQ")
+        self.controller.gpib_usb.write("SYST:ZCH OFF")
+        self.controller.gpib_usb.write("SYST:ZCOR ON")
+        
+    def do_one_reading(self):
+        self.controller.gpib_usb.write("*RST")
+        self.controller.gpib_usb.write("SYST:ZCH ON")
+        self.controller.gpib_usb.write("CURR:RANG 2E-9")
+        self.controller.gpib_usb.write("INIT")
+        self.controller.gpib_usb.write("SYST:ZCOR:ACQ")
+        self.controller.gpib_usb.write("SYST:ZCOR ON")
+        self.controller.gpib_usb.write("CURR:RANG:AUTO ON")
+        self.controller.gpib_usb.write("SYST:ZCH OFF")
+        data = self.controller.gpib_usb.query("READ?")
+        data = data.strip()
+        logging.debug("Current: %s", data)
+        return data
+        
     def identification(self):
         self.controller.gpib_usb.write("*IDN?")
         data = self.controller.gpib_usb.ask("++read eoi")
